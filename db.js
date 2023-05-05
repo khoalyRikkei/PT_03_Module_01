a8140e542ee0c75b2306d4856e962e07993a1b4d
LÊ Y KHOA <leykhoaqk@gmail.com>
2023-05-05 23:28:49 +0700

    ES6

---
+++ db.js
@@ -0,0 +1,6 @@
+const db = [
+  { name: "hocsinh_1", age: 18 },
+  { name: "hocsinh_2", age: 18 },
+];
+
+export default db;
---
+++ db2.js
@@ -0,0 +1,14 @@
+const sp1 = [
+  { name: "hocsinh_1", age: 18 },
+  { name: "hocsinh_2", age: 18 },
+];
+const sp2 = [
+  { name: "hocsinh_1", age: 18 },
+  { name: "hocsinh_2", age: 18 },
+];
+const sp3 = [
+  { name: "hocsinh_1", age: 18 },
+  { name: "hocsinh_2", age: 18 },
+];
+
+export { sp1, sp2, sp3 };
